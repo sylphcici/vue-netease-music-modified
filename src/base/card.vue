4817fdf08f1e025ef108996b37e41a71aa243ad2
sylphcici <sylphcici@163.com>
2026-04-08 21:46:29 +0800

    Update card.vue

--- src/base/card.vue
+++ src/base/card.vue
@@ -1,5 +1,11 @@
 <template>
   <div @click="onClick" class="horizontal-card">
+    <slot name="img-wrap">
+      <div class="img-wrap">
+        <img v-lazy="$utils.genImgUrl(img, 50)" />
+        <slot name="img-mask"></slot>
+      </div>
+    </slot>
     <div class="content">
       <div class="name">{{ name }}</div>
       <div class="desc">
