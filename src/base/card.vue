186787b0457aec63f28f9985f9bafc0d50e8428e
sylphcici <sylphcici@163.com>
2026-04-08 21:53:31 +0800

    Update card.vue

--- src/base/card.vue
+++ src/base/card.vue
@@ -9,7 +9,7 @@
     <div class="content">
       <div class="name">{{ name }}</div>
       <div class="desc">
-        <slot name="desc">{{ desc }}</slot>
+        <slot name="desc">test</slot>
       </div>
     </div>
   </div>
